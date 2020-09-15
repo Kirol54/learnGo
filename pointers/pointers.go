f63257672d7e4785a16f175c57ead14a5d1211fc
Karol Podufalski <karol@extropy.io>
2020-09-15 19:13:19 +0100

    EP 03 finished.

---
+++ pointers/pointers.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type position struct {
+	x float32
+	y float32
+}
+
+type badGuy struct {
+	name   string
+	health int
+	pos    position
+}
+
+func addOne(num *int) {
+	*num = *num + 1
+}
+
+func whereIsBadGuy(b *badGuy) {
+	x := b.pos.x
+	y := b.pos.y
+	fmt.Println("(", x, ",", y, ")")
+}
+
+func main() {
+	x := 5
+	var xPtr *int = &x
+	fmt.Println(xPtr)
+	addOne(xPtr)
+	fmt.Println(x)
+
+	p := position{4, 2}
+	badGuy := badGuy{"Jabba", 100, p}
+	whereIsBadGuy(&badGuy)
+}
